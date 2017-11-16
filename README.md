9f27d3f47307051455643b231797583912a1e14d
Sumeet Sarkar <sumeetsarkar4@gmail.com>
2017-11-16 20:31:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# xmllint demo
+
+> A simple bash script to explore xmllint usage
+
+[xmllint Documentation](http://www.xmlsoft.org/xmllint.html)<br />
+
+### Installation
+Ubuntu, run apt-get install libxml2-utils (if not installed already)<br />
+Mac OS X (Yosemite), installed by default
+
+### xmllint usage demos in this project
+1. --xpath
+
+### Running the demo
+
+Project includes below sample xml files<br/>
+[books.xml](data/books.xml)<br/>
+[food.xml](data/food.xml)<br/>
+
+### Make app.sh executable and run the script
+    ./app.sh data/food.xml
+    ./app.sh data/books.xml
---
+++ app.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function main() {
+  # check if file is food.xml
+  if [ $1 == 'data/food.xml' ]; then
+    processBreakfastMenu $1
+  # check if file is books.xml
+  elif [ $1 == 'data/books.xml' ]; then
+    processBookstore $1
+  fi
+}
+
+function processBreakfastMenu() {
+  echo $'usage of --path /breakfast_menu/food[1]\n'
+  xmllint --xpath '/breakfast_menu/food[1]' $1
+}
+
+function processBookstore() {
+  echo $'usage of --path /bookstore/book[@category="cooking"]/author/text()\n'
+  xmllint --xpath '/bookstore/book[@category="cooking"]/author/text()' $1
+}
+
+echo 'File being read -' $1
+echo $'Output...\n'
+
+main $1
+
+echo $'\n\n.........'
